f7ed53c60c6bb4c2398c45d8ff56f28fe7c0dfab
Trevor Nichols <trevor@theredfox.group>
2020-07-28 08:18:20 +1000

    Add two utility commands to list and extract meg files

---
+++ OpenRA.Mods.Cnc/UtilityCommands/ExtractMegContentsCommand.cs
@@ -0,0 +1,56 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.IO;
+using OpenRA.Mods.Cnc.FileSystem;
+
+namespace OpenRA.Mods.Cnc.UtilityCommands
+{
+	class ExtractMegContentsCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--extract-meg"; } }
+
+		bool IUtilityCommand.ValidateArguments(string[] args)
+		{
+			return args.Length == 3;
+		}
+
+		[Desc(@"D:\SteamLibrary\steamapps\common\CnCRemastered\Data\music.meg", @"D:\exported", "Extracts a meg file into the target directory")]
+		void IUtilityCommand.Run(Utility utility, string[] args)
+		{
+			var source = args[1];
+			var dest = args[2];
+
+			if (!Directory.Exists(dest))
+				Directory.CreateDirectory(dest);
+
+			var package = new MegV3Loader.MegFile(File.OpenRead(source), source);
+
+			foreach (var entry in package.Contents)
+			{
+				Console.WriteLine("{0}", entry);
+
+				var baseDir = Path.Combine(dest, Path.GetDirectoryName(entry));
+				if (!Directory.Exists(baseDir))
+					Directory.CreateDirectory(baseDir);
+
+				using (var destStream = File.OpenWrite(Path.Combine(dest, entry)))
+				{
+					var sourceStream = package.GetStream(entry);
+					sourceStream.CopyTo(destStream);
+				}
+			}
+
+			Console.WriteLine("All files extracted to {0}", dest);
+		}
+	}
+}
---
+++ OpenRA.Mods.Cnc/UtilityCommands/ListMegContentsCommand.cs
@@ -0,0 +1,42 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using OpenRA.Mods.Cnc.FileFormats;
+using OpenRA.Mods.Cnc.FileSystem;
+
+namespace OpenRA.Mods.Cnc.UtilityCommands
+{
+	class ListMegContentsCommand : IUtilityCommand
+	{
+		string IUtilityCommand.Name { get { return "--list-meg"; } }
+
+		bool IUtilityCommand.ValidateArguments(string[] args)
+		{
+			return args.Length == 2;
+		}
+
+		[Desc(@"D:\SteamLibrary\steamapps\common\CnCRemastered\Data\music.meg", "Lists the content ranges for a meg file")]
+		void IUtilityCommand.Run(Utility utility, string[] args)
+		{
+			var package = new MegV3Loader.MegFile(File.OpenRead(args[1]), args[1]);
+			foreach (var kv in package.Index)
+			{
+				Console.WriteLine("{0}:", kv.Key);
+				Console.WriteLine("\tOffset: {0}", kv.Value.Offset);
+				Console.WriteLine("\tLength: {0}", kv.Value.Length);
+			}
+		}
+	}
+}
